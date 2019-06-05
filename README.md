5517132e78b35e3fc3e78e462669fe6c1f4f5dbc
Leo Farias <leo@conceptsol.com>
2019-06-05 01:24:17 -0400

    Initial commmit of inspector

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# zeit-integrations-inspect
+# zeit-integration-inspect
+
+Quickly inspect values during Zeit Integrations development.
+
+![Screenshot](./screenshot.png)
+
+## Install
+
+```bash
+yarn add --dev zeit-integration-inspect
+```
+
+## Usage
+
+```javascript
+const inspectPanel = require("zeit-integration-inspect");
+
+async function handler(options){
+    return htm`
+        <Page>
+            <Container>
+                <H2>Your Integration</H2>
+                <P>Awesome description.</P>
+            </Container>
+            ${await inspectPanel(options, values)}
+        </Page>
+    `;
+}
+
+module.exports = withUiHook(handler);
+```
+
+**inspectPanel** accepts 2 parameters.
+
+- **options** - values that are passed down from withUiHook middleware. i.e. *metadata, clientState, action and etc*.
+
+- **values** - These are values you would like to display during development.
---
+++ index.js
@@ -0,0 +1,104 @@
+const { htm } = require("@zeit/integration-utils");
+
+module.exports = async function inspectPanel(options, values) {
+  let { payload, metadata, zeitClient } = options;
+  let { action, clientState } = payload;
+
+  // Sets default values to inspect
+  values["ClientState"] = clientState;
+  values["Metadata"] = metadata;
+  values["Action"] = action;
+
+  // Gets keys of all the values
+  const keys = Object.keys(values);
+
+  // If gets saved values or sets empty object
+  let inspectorValues = metadata.inspectorValues || {};
+
+  // If a key does not exist, create with default to show
+  keys.forEach(k => {
+    if (!inspectorValues[k]) {
+      inspectorValues[k] = "SHOW";
+    }
+  });
+
+  if (action === "hide-inspector") {
+    metadata["showInspector"] = false;
+  }
+
+  if (action === "show-inspector") {
+    metadata["showInspector"] = true;
+  }
+
+  if (action === "refresh-inspector") {
+    // Checks values and updates metadata
+    keys.forEach(k => {
+      inspectorValues[k] = clientState["inspect-" + k] ? "SHOW" : "HIDE";
+    });
+  }
+
+  metadata.inspectorValues = inspectorValues;
+
+  if (action === "clear-metadata-inspector") {
+    metadata = {};
+  }
+
+  // Sets sets updated values to metadata
+  await zeitClient.setMetadata(metadata);
+
+  // Check if it should display inspector, and sets css property
+  const shouldDisplay = metadata.showInspector ? "visible" : "hidden";
+
+  // Render inspector button to show and hide based on showInspector
+  const renderDisplayButton = metadata.showInspector
+    ? htm`<Button small action="hide-inspector">Hide</Button>`
+    : htm`<Button small action="show-inspector">Show Inspector</Button>`;
+
+  // Options to toggle the display of the panels
+  const renderDisplayOptions = keys => {
+    return keys.map(k => {
+      return htm`
+        <Box margin-left="20px">
+            <Checkbox name="${"inspect-" + k}" label="${k}" checked="${inspectorValues[k] === "SHOW"}"  />
+        </Box>
+        `;
+    });
+  };
+
+  // renders the panels for each value to inspect
+  const renderPanels = keys => {
+    return keys.map(k => {
+      // Loop through all the value that are set to show to create the panels
+      if (inspectorValues[k] === "SHOW") {
+        return htm`
+        <Box width="100%" overflow="auto" border="1px solid #222">
+            <Box position="absolute" height="40px" padding="10px" background-color="rgb(37, 37, 37)">${k}</Box>
+            <Box white-space="pre" font-size="14px" margin-top="40px" line-height="24px" padding="20px" font-family="monospace, serif">
+                ${JSON.stringify(values[k], null, 2)}
+            </Box>
+        </Box>`;
+      } else {
+        // If value is not set to show, create empty box
+        return htm`<Box/>`;
+      }
+    });
+  };
+
+  return htm`
+      <Box visibility="${shouldDisplay}" display="flex" width="100%" z-index="998" right="0" left="0" max-height="500px"  position="fixed" bottom="40px" width="100%" color="#fff" background-color="#010101"> 
+        ${renderPanels(keys)}
+      </Box>
+      <Box border-top="1px solid #111" position="fixed" z-index="999" width="100%" left="0" right="0" height="45px" bottom="0" padding="10px" background-color="#111">
+        <Box display="flex" align-items="center" color="#fff">  
+          ${renderDisplayButton}
+          <Box margin-left="10px">
+            <Button small abort action="clear-metadata-inspector">Clear Metadata</Button>     
+          </Box>
+          ${renderDisplayOptions(keys)}
+          <Box margin-left="10px">
+            <Button small action="refresh-inspector">Update display</Button>     
+          </Box>
+        </Box>
+      </Box>
+    `;
+};
